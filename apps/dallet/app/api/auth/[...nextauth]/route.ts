52b2eade77945a2218fd2ae3a75ec46d91721aaa
sharathxct <sharathxct@gmail.com>
2024-08-30 12:07:49 +0530

    type script error

--- apps/dallet/app/api/auth/[...nextauth]/route.ts
+++ apps/dallet/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import { authConfig } from "../../../../lib/auth";
 import NextAuth from "next-auth"
 
+//@ts-ignore
 const handler = NextAuth(authConfig)
 
 export { handler as GET, handler as POST }
